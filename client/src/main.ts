efd5133c90b62475ff02e560c55be988697818aa
John Pier <j0hnpier.key@gmail.com>
2021-04-05 19:10:22 +0400

    update

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,9 @@
 import {enableProdMode} from "@angular/core";
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import "hammerjs";
-
 import {AppModule} from "./app/app.module";
 import {environment} from "./environments/environment";
-
 import {hmrBootstrap} from "./hmr";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
 if (environment.production) {
     enableProdMode();
